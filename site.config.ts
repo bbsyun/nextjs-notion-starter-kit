6165881a4235e785587f9661119f2cf0d986469c
bbsyun <161267868+bbsyun@users.noreply.github.com>
2026-04-16 15:44:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Bosyunn Itinerary',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Bosyunn',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
